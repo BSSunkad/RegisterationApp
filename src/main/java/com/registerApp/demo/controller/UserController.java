7db7ef34efb1ef0650e6941292064db95fdc91d6
BSSunkad <bsunkadbasavaraj@gmail.com>
2023-10-06 18:42:49 +0530

    first

--- src/main/java/com/registerApp/demo/controller/UserController.java
+++ src/main/java/com/registerApp/demo/controller/UserController.java
@@ -19,10 +19,10 @@ import lombok.extern.slf4j.Slf4j;
 @RestController
 public class UserController {
 
-	@Autowired  //asbhk
+	@Autowired  
 	private UserService userService;
 
-	// abhishek
+	// abhishek created git repository
 	@PostMapping("/register")
 	public ResponseEntity<String> register(@RequestBody RegisterDto registerDto) {
 		log.info("In register-app Register started");
